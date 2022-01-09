353fcbd1a8775dd2019fb3ff25c7785d90b4f0df
Matt Krause <krause.matt@pm.me>
2022-01-08 20:14:08 -0500

    Creating React eCommerce app, initialized project and finished navbar using Bootstrap

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class NavBar extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <nav className="navbar navbar-expand-md navbar-dark bg-primary">
+          <a className="navbar-brand" href="/#">
+            <img src="images/pizza.svg" width="40" height="40" className="d-inline-block align-top" alt="" />
+            Pizza Store
+          </a>
+
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+
+          <div className="collapse navbar-collapse font-weight-bold" id="navbarSupportedContent">
+            <ul className="navbar-nav mr-auto ml-auto">
+              <li className="nav-item active">
+                <a className="nav-link" href="/#">Menu</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#">Cart</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#">Checkout</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </React.Fragment>
+      
+    )
+  }
+}
+
+export default NavBar;
