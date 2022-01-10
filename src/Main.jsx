7cc18a1d8f8627072c0cca64194aaeb406aa2c0e
Matt Krause <krause.matt@pm.me>
2022-01-09 19:05:01 -0500

    Added badge to display customer list number using state and delete button to modify customer list state

--- src/Main.jsx
+++ src/Main.jsx
@@ -3,14 +3,14 @@ import "./index.css";
 
 class Main extends Component {
   state = {
-    custCount: 0,
     custList: [
       {id: 1, order: "https://source.unsplash.com/random/?sausage+pizza", last: "Crede", first: "Joe", email: "jcrede@fake.com", phone: "(555) 242-4242" },
       {id: 2, order: "https://source.unsplash.com/random/?pepperoni+pizza", last: "Thomas", first: "Frank", email: "fthomas@fake.com", phone: "(555) 353-5353" },
       {id: 3, order: "https://source.unsplash.com/random/?veggie+pizza", last: "Buehrle", first: "Mark", email: "mbuehrle@fake.com", phone: "(555) 565-6565" },
       {id: 4, order: "https://source.unsplash.com/random/?pizza", last: "Podsednik", first: "Scott", email: "spodsednik@fake.com", phone: "(555) 222-2222" },
       {id: 5, order: "https://source.unsplash.com/random/?sausage+pizza", last: "Konerko", first: "Paul", email: "pkonerko@fake.com", phone: "(555) 141-4141" }
-    ]
+    ],
+    custCount: 5
   }
 
   render() {
@@ -22,12 +22,13 @@ class Main extends Component {
           <table className="table table-bordered table-hover" id="custTable">
             <thead className="thead-light">
               <tr>
-                <th scope="col">#</th>
-                <th scope="col">Order</th>
-                <th scope="col">Last Name</th>
-                <th scope="col">First Name</th>
-                <th scope="col">E-Mail</th>
-                <th scope="col">Phone</th>
+                <th scope="col" className="text-center">#</th>
+                <th scope="col" className="text-center">Order</th>
+                <th scope="col" className="text-center">Last Name</th>
+                <th scope="col" className="text-center">First Name</th>
+                <th scope="col" className="text-center">E-Mail</th>
+                <th scope="col" className="text-center">Phone</th>
+                <th scope="col" className="text-center">Delete?</th>
               </tr>
             </thead>
             <tbody>
@@ -45,20 +46,29 @@ class Main extends Component {
 
   getCustRow = () => {
     return (
-      this.state.custList.map((customer) => {
+      this.state.custList.map((customer, index) => {
         return (
           <tr key={customer.id}>
-            <th scope="row">{customer.id}</th>
+            <th className="text-center align-middle" scope="row">{customer.id}</th>
             <td><img className="img-fluid img-thumbnail mx-auto d-block" style={{height: "150px", width: "150px", objectFit: "cover"}} src={customer.order} alt="order"/></td>
-            <td>{customer.last}</td>
-            <td>{customer.first}</td>
-            <td>{customer.email}</td>
-            <td>{customer.phone}</td>
+            <td className="text-center align-middle">{customer.last}</td>
+            <td className="text-center align-middle">{customer.first}</td>
+            <td className="text-center align-middle">{customer.email}</td>
+            <td className="text-center align-middle">{customer.phone}</td>
+            <td className="text-center align-middle"><button className="btn btn-danger" onClick={() => this.deleteRow(index)}>Delete</button></td>
+
           </tr>
         )
       })
     )
   }
+
+  deleteRow = (index) => {
+    const newCustList = this.state.custList;
+    newCustList.splice(index, 1);
+    this.setState({custList: newCustList});
+    this.setState({custCount: newCustList.length})
+  };
 };
 
 export default Main;
