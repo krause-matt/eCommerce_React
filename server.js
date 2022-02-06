b49c384f802cc1d7561e0933c75868ec0728326b
Matt Krause <krause.matt@pm.me>
2022-02-06 17:12:11 -0500

    Set server.js port variable to url + process env PORT

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = "https://ecommerce-pizza-place.herokuapp.com";
+const port = `https://ecommerce-pizza-place.herokuapp.com:${process.env.PORT}`;
 
 server.use(middlewares)
 server.use(router)
