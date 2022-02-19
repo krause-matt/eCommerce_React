3a337ec18d3656f2164894a801bbda6df1d425e5
Matt Krause <krause.matt@pm.me>
2022-02-19 12:53:23 -0500

    Forgot to import Link from react-router-dom

--- src/Success.jsx
+++ src/Success.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import "./index.css";
 import orderServer from "./api/orders";
 import orders from "./api/orders";
